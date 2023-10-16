3033215d77fd269fa48469ff4ffbf96a3dd88066
Joan D GG <ggjoan2000@gmail.com>
2023-10-15 21:24:51 -0600

    Fixed sincronization

--- Assets/Scripts/Character/isGrounded.cs
+++ Assets/Scripts/Character/isGrounded.cs
@@ -3,21 +3,19 @@ using UnityEngine;
 public class isGrounded : MonoBehaviour
 {
     public ComponentsManager componentsManager;
-    private CharacterStats stats;
 
-    void Start() => stats = componentsManager.characterStats;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Stage") || other.CompareTag("Platform"))
         {
-            stats.setIsGrounded(true);
-            stats.jumpsUsed = 0;
+            componentsManager.characterStats.setIsGrounded(true);
+            componentsManager.characterStats.jumpsUsed = 0;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Stage") || other.CompareTag("Platform"))
-            stats.setIsGrounded(false);
+            componentsManager.characterStats.setIsGrounded(false);
     }
 }
--- Assets/Scripts/Stage/Blastzone/Blastzone.cs
+++ Assets/Scripts/Stage/Blastzone/Blastzone.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Blastzone : MonoBehaviour
 {
-
     public float height = 15;
     public float width = 15;
     public GameObject leftWall;
@@ -18,15 +15,5 @@ public class Blastzone : MonoBehaviour
         rightWall.transform.position = new Vector3(width, 0f, 0f);
         ceiling.transform.position = new Vector3(0f, height, 0f);
         floor.transform.position = new Vector3(0f, -height, 0f);
-    }
-
-    public void respawnCharacter(GameObject character)
-    {
-        CharacterStats stats = character.GetComponent<ComponentsManager>().characterStats;
-        Rigidbody rb = character.GetComponent<ComponentsManager>().rigidbody;
-
-        stats.inHitStun = false;
-        character.transform.position = new Vector3(0.0f, 5.0f, 0.0f);
-        rb.velocity = Vector3.zero;
     }
 }
