188ca0f48d751122f40573995b2fe2e796f35b6e
Joan D GG <ggjoan2000@gmail.com>
2023-10-22 23:19:20 -0600

    Animations and sounds

--- Assets/Scripts/Stage/Blastzone/BlastzoneWalls.cs
+++ Assets/Scripts/Stage/Blastzone/BlastzoneWalls.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class BlastzoneWalls : MonoBehaviour
 {
-    Blastzone blastzone;
+    public AudioSource audioSource;
     public GameObject deathEffect;
-    void Start() => blastzone = transform.parent.GetComponent<Blastzone>();
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            audioSource.Play();
             CharacterStats stats = other.gameObject.GetComponent<ComponentsManager>().characterStats;
             Instantiate(deathEffect, other.transform.position, Quaternion.identity);
             stats.loseStock();
