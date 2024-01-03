1a15eca93e4eb60f526b468a9eff06b56ca86b7b
Daniel <danielttgb44@live.com.mx>
2024-01-02 20:30:56 -0600

    Continue on the menu ui

--- Assets/Scripts/UI/ButtonsNavigation.cs
+++ Assets/Scripts/UI/ButtonsNavigation.cs
@@ -3,15 +3,15 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;// Required when using Event data.
 
-public class ButtonsNavigation : MonoBehaviour, ISelectHandler, IDeselectHandler // required interface when using the OnSelect method.
+public class ButtonsNavigation : MonoBehaviour
 {
-    public void OnSelect(BaseEventData eventData)
+    public void OnHover()
     {
         transform.GetChild(1).gameObject.SetActive(true);
         transform.GetChild(2).gameObject.SetActive(true);
     }
 
-    public void OnDeselect(BaseEventData data)
+    public void OffHover()
     {
         transform.GetChild(1).gameObject.SetActive(false);
         transform.GetChild(2).gameObject.SetActive(false);
