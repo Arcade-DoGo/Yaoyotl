833093fe81d9b90028169a26222fd45be726faf8
Daniel <danielttgb44@live.com.mx>
2024-01-24 23:41:25 -0600

    Connect UI stats for gameplay pt. 1

--- Assets/Scripts/UI/MatchData.cs
+++ Assets/Scripts/UI/MatchData.cs
@@ -5,17 +5,50 @@ using TMPro;
 public class MatchData : MonoBehaviour
 {
     public static MatchData instance;
-    public TextMeshProUGUI[] stocks, playerDamages;
+    public GameObject[] PlayerStatsUIList;
     public TextMeshProUGUI timer;
     private int minutes, seconds;
     private void Awake() => instance = this;
     public void UpdatePlayersData(CharacterStats stats)
     {
         // Debug.Log("PROPERTIES UPDATE: " + stats.playerName + " " + stats.stocks + " " + stats.damage);
+        /*
         int playerIndex = GameManager.players.IndexOf(stats);
         stocks[playerIndex].text = stats.stocks + "";
         playerDamages[playerIndex].text = stats.damage + "%";
+        */
+        int playerIndex = GameManager.players.IndexOf(stats);
+
+        TextMeshProUGUI damageTxt = PlayerStatsUIList[playerIndex].transform.Find("DamageTxt").GetComponent<TextMeshProUGUI>();
+        damageTxt.text = stats.damage + "%";
+
+        GameObject marcoIconoGameObject = PlayerStatsUIList[playerIndex].transform.Find("MarcoIcono").gameObject;
+        GameObject stock1 = marcoIconoGameObject.transform.Find("PlayerStock_3").gameObject;
+        GameObject stock2 = marcoIconoGameObject.transform.Find("PlayerStock_2").gameObject;
+        GameObject stock3 = marcoIconoGameObject.transform.Find("PlayerStock_3").gameObject;
+
+        switch (stats.stocks)
+        {
+            case 1:
+                stock1.SetActive(true);
+                stock2.SetActive(false);
+                stock3.SetActive(false);
+                break;
+            case 2:
+                stock1.SetActive(true);
+                stock2.SetActive(true);
+                stock3.SetActive(false);
+                break;
+            case 3:
+                stock1.SetActive(true);
+                stock2.SetActive(true);
+                stock3.SetActive(true);
+                break;
+            default:
+                break;
+        }
         if(stats.stocks == 0) GameplayManager.instance.GameOver(GameManager.players.Find(player => player != stats));
+        
     }
     public void StartTimer() => StartCoroutine(UpdateTimer());
     private IEnumerator UpdateTimer()
